089dfddfabeed6dbac015192a31abe363033601e
Heriberto Yañez <169105430+Herivasm@users.noreply.github.com>
2025-06-18 08:55:32 -0700

    Modificación al modelo de Productos

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,9 +1,25 @@
-import mongoose, { Document, Schema, Types } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+
+export const productCategories = {
+    HOT_DRINKS: 'hotDrinks',
+    COLD_DRINKS: 'coldDrinks',
+    ALCOHOL: 'alcohol',
+    SNACKS: 'snacks',
+    HAMBURGUERS: 'hamburguers',
+    BAGGUETTES: 'baguettes',
+    SANDWICHES: 'sandwiches',
+    DESSERTS: 'desserts'
+} as const
+
+export type ProductCategory = typeof productCategories[keyof typeof productCategories]
 
 export interface IProduct extends Document {
-    productName: string,
-    price: number,
+    productName: string
+    price: number
+    cost: number
     description: string
+    photoUrl?: string
+    category: ProductCategory
 }
 
 const ProductSchema: Schema = new Schema({
@@ -16,10 +32,23 @@ const ProductSchema: Schema = new Schema({
         type: Number,
         required: true
     },
+    cost: {
+        type: Number,
+        required: true
+    },
     description: {
         type: String,
         required: true,
         trim: true,
+    },
+    photoUrl: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    category: {
+        type: String,
+        enum: Object.values(productCategories)
     }
 }, { timestamps: true })
 
