bc2604c18e2be04bcbdac0a9ff43dd06b71b386b
Heriberto Yañez <169105430+Herivasm@users.noreply.github.com>
2025-06-25 20:46:55 -0700

    Modificación a la configuración del servidor

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db";
 import productRoutes from "./routes/productRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import supplierRoutes from "./routes/supplierRoutes";
-import supplierRoutes from "./routes/supplierRoutes";
+import supplyRoutes from "./routes/supplyRoutes";
 
 dotenv.config()
 connectDB()
 
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use(express.json())
 
